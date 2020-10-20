b408b8518adf2b3a66c9984fe19c2e1a521e4f15
Alexander Kozlov <alex.d.kozlov@gmail.com>
2020-10-20 17:23:27 +0300

    fixes. added start-date parameter

--- cost_alert/lambda_application/lambda_function.py
+++ cost_alert/lambda_application/lambda_function.py
@@ -23,16 +23,13 @@ def read_s3_file():
     )
     s3_key = os.environ['S3_KEY']
     s3_bucket = os.environ['S3_BUCKET']
-    print(s3_key)
     obj = s3.Object(s3_bucket, s3_key)
     if key_exists(s3_client, s3_key, s3_bucket):
         from_s3 = obj.get()['Body'].read().decode('utf-8')
         print('downloaded file from s3')
-        print(from_s3)
-        print(type(from_s3))
         return from_s3
     else:
-        print(f"Could not find a file in s3.")
+        print(f"Could not find a file in S3.")
 
 
 def key_exists(s3_client, mykey, mybucket):
@@ -44,7 +41,7 @@ def key_exists(s3_client, mykey, mybucket):
     return False
 
 
-def read_last_100(prev_responses, need_length):
+def read_last_100(prev_responses, start_date):
     client = boto3.client(
         'ce',
         aws_access_key_id=os.environ['ACCESS'],
@@ -55,21 +52,39 @@ def read_last_100(prev_responses, need_length):
     now_date = datetime.now()
     now_date_string = now_date.strftime("%Y-%m-%d")  # TODAY
 
-    if len(prev_responses) < need_length:  # by default should be == 100
+    input_start_date = datetime.strptime(start_date, "%Y-%m-%d")  # '2020-10-18'
+    today_date = datetime.strptime(now_date_string, "%Y-%m-%d")
+    delta = abs((today_date - input_start_date).days)
 
-        previous_date = now_date - 100 * timedelta(hours=24)
-        previous_date_string = previous_date.strftime("%Y-%m-%d")
+    if len(prev_responses) < 100:  # by default should be == 100
+        if delta < 100:
+            previous_date = now_date - delta * timedelta(hours=24)
+            previous_date_string = previous_date.strftime("%Y-%m-%d")
 
-        response = client.get_cost_and_usage(TimePeriod={
-            'Start': f'{previous_date_string}',
-            'End': f'{now_date_string}'
-        }, Granularity='DAILY',
-            Metrics=['NetAmortizedCost'])
+            response = client.get_cost_and_usage(TimePeriod={
+                'Start': f'{previous_date_string}',
+                'End': f'{now_date_string}'
+            }, Granularity='DAILY',
+                Metrics=['NetAmortizedCost'])
 
-        new_prev_responses = json.loads(str(json.dumps(response)))['ResultsByTime']
-        print(len(new_prev_responses))
-        print('type of response =', type(new_prev_responses), 'new response=', new_prev_responses)
-        return new_prev_responses
+            new_prev_responses = json.loads(str(json.dumps(response)))['ResultsByTime']
+            print(len(new_prev_responses))
+            print('new response=', new_prev_responses)
+            return new_prev_responses
+        else:
+            previous_date = now_date - 100 * timedelta(hours=24)
+            previous_date_string = previous_date.strftime("%Y-%m-%d")
+
+            response = client.get_cost_and_usage(TimePeriod={
+                'Start': f'{previous_date_string}',
+                'End': f'{now_date_string}'
+            }, Granularity='DAILY',
+                Metrics=['NetAmortizedCost'])
+
+            new_prev_responses = json.loads(str(json.dumps(response)))['ResultsByTime']
+            print('len of new response =', len(new_prev_responses))
+            print('new response =', new_prev_responses)
+            return new_prev_responses
     else:
         previous_date = now_date - timedelta(hours=24)
         previous_date_string = previous_date.strftime("%Y-%m-%d")
@@ -81,28 +96,25 @@ def read_last_100(prev_responses, need_length):
             Metrics=['NetAmortizedCost'])
 
         resp = json.loads(str(json.dumps(response)))['ResultsByTime'][0]
-        print(type(resp), 'new response = ', resp)
-        print(type(prev_responses), 'prev resp =', prev_responses)
+        print('new response =', resp)
+        print('prev resp =', prev_responses)
         new_prev_responses = json.loads(prev_responses)
-        print(type(new_prev_responses), 'after loads =', new_prev_responses)
         if resp['TimePeriod']['Start'] == new_prev_responses[-1]['TimePeriod']['Start'] and \
                 resp['TimePeriod']['End'] == new_prev_responses[-1]['TimePeriod']['End']:
             new_prev_responses[-1] = resp
             print('Last record is updated')
-            print('len of new_previous =', len(new_prev_responses))
+            print('len of updated response =', len(new_prev_responses))
             return new_prev_responses
         else:
             new_prev_responses.append(resp)
-            print('after-append', new_prev_responses)
+            print('after append', new_prev_responses)
             del new_prev_responses[0]
             print('len of new_previous =', len(new_prev_responses))
-            print('type of response =', type(new_prev_responses), 'edited response =', new_prev_responses)
+            print('edited response =', new_prev_responses)
             return new_prev_responses
 
 
 def analyze_w_last100(last_100_list):
-    print(last_100_list)
-    print(type(last_100_list))
     df_start = pd.DataFrame((obj['TimePeriod']['Start'],
                              obj['Total']['NetAmortizedCost']['Amount']) for obj in last_100_list)
 
@@ -112,81 +124,79 @@ def analyze_w_last100(last_100_list):
     last_response_month = str(df_start.iloc[int(df_start.shape[0] - 1)]['start'][0:-3])
 
     df_start_first_only = df_start[df_start['start'].str.endswith('01')].reset_index(drop=True)
-    if df_start_first_only.shape[0] > 1:
-        df_start_first_only = df_start_first_only.drop(df_start_first_only.shape[0] - 1)
-    avg_start_only = df_start_first_only['amount'].mean()
+    avg_start_only = df_start_first_only.iloc[:-1]['amount'].mean()
 
     df_not_first = df_start[~df_start['start'].str.endswith('01')].reset_index(drop=True)
-    if df_not_first.shape[0] > 1:
-        df_not_first = df_not_first.drop(df_not_first.shape[0] - 1)
-    avg_not_start = df_not_first['amount'].mean()
+    avg_not_start = df_not_first.iloc[:-1]['amount'].mean()
 
     last_response_amount = last_response['amount']
 
     if last_response['start'].endswith('01') is True:
         if df_start_first_only.loc[int(df_start_first_only.shape[0] - 1)]['amount']:
-            message = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")) + '\nNew S3 spends request.' + \
-                      '\n\nYesterday ' + str(df_start_first_only.loc[int(df_start_first_only.shape[0] - 1)]['start']) + \
-                      ' spent: {}'.format(
-                          str(round(float(df_start_first_only.loc[int(df_start_first_only.shape[0] - 1)]['amount']),
-                                    2))) + \
-                      ' USD. (First day of the month)' + \
-                      '\n\n1st days of month: ' + \
-                      str(df_start_first_only.loc[int(df_start_first_only.shape[0] - 2)]['start']) + '. Spent: ' + \
-                      str(round(float(df_start_first_only.loc[int(df_start_first_only.shape[0] - 2)]['amount']), 2)) + \
-                      '\nMean for this month {}: {}'.format(last_response_month,
-                                                                round(float(df_start[df_start['start'].str.contains(str(
-                                                                    df_start.iloc[int(df_start.shape[0]) - 1]['start'][
-                                                                    0:-3]))].mean()), 2)) + ' USD' + \
-                      '\nMean for prev 1st days of months: ' + str(round(float(avg_start_only), 2))
+            today_date = str(df_start_first_only.loc[int(df_start_first_only.shape[0] - 1)]['start'])
+            today_spent = str(round(float(df_start_first_only.loc[int(df_start_first_only.shape[0] - 1)]['amount']),
+                                    2))
+            prev_date = str(df_start_first_only.loc[int(df_start_first_only.shape[0] - 2)]['start'])
+            prev_spent = str(round(float(df_start_first_only.loc[int(df_start_first_only.shape[0] - 2)]['amount']), 2))
+            month_mean = round(float(df_start[df_start['start'].str.contains(str(
+                df_start.iloc[int(df_start.shape[0]) - 1]['start'][
+                0:-3]))].mean()), 2)
+            mean_prev_first = str(round(float(avg_start_only), 2))
+            at_moment_time = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            len_last_100_first = int(df_start_first_only.shape[0])
+
+            message = f"{at_moment_time} \nNew S3 spends request. \n\nYesterday {today_date} spent: {today_spent}" \
+                f" USD. (First day of the month) \nIn previous 1st days of month: {prev_date}. Spent: {prev_spent} USD" \
+                f"\nMean for this month {last_response_month}: {month_mean} USD" \
+                f"\nMean for {len_last_100_first} prev 1st days of months: {mean_prev_first} USD."
             send_slack_message(message, 0)
             send_email_message(message)
             if last_response_amount > avg_start_only * 1.1:
                 if last_response_amount > avg_start_only * 1.2:
-                    alarm_message = message + '\n\nAlarm! average begin month daily limit {} USD exceeded higher 20%, by {}%, now –– {} USD'.format(
-                        avg_start_only, round(float((last_response_amount / avg_start_only) * 100 - 100), 2),
-                        last_response_amount)
+                    exceed_percent = round(float((last_response_amount / avg_start_only) * 100 - 100), 2)
+                    alarm_message = message + f"\n\nAlarm! average begin month daily limit {avg_start_only} USD " \
+                        f"exceeded higher 20%, by {exceed_percent}%, now –– {last_response_amount} USD."
                     send_slack_message(alarm_message, 1)
                     send_email_message(alarm_message)
                 else:
-                    warning_message = message + '\n\nWarning! average begin month daily limit {} USD exceeded higher 10%, by {}%, now –– {} USD'.format(
-                        avg_start_only, round(float((last_response_amount / avg_start_only) * 100 - 100), 2),
-                        last_response_amount)
+                    exceed_percent = round(float((last_response_amount / avg_start_only) * 100 - 100), 2)
+                    warning_message = message + f"\n\nWarning! average begin month daily limit {avg_start_only} USD " \
+                        f"exceeded higher 10%, by {exceed_percent}%, now –– {last_response_amount} USD."
                     send_slack_message(warning_message, 1)
                     send_email_message(warning_message)
     else:
         if df_not_first.loc[int(df_not_first.shape[0] - 1)]['amount']:
-            message = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")) + '\nNew S3 spends request.' + \
-                      '\n\nYesterday ' + str(df_not_first.loc[int(df_not_first.shape[0] - 1)]['start']) + \
-                      ' spent: {}'.format(
-                          str(round(float(df_not_first.loc[int(df_not_first.shape[0] - 1)]['amount']), 2))) + \
-                      ' USD. (Not first day of the month)' + \
-                      '\nDay before yesterday: ' + str(df_not_first.loc[int(df_not_first.shape[0] - 2)]['start']) + \
-                      ' spent: {}'.format(
-                          str(round(float(df_not_first.loc[int(df_not_first.shape[0] - 2)]['amount']), 2))) + \
-                      ' USD' + \
-                      '\nMean for this month {}: {}'.format(last_response_month,
-                                                                round(float(df_start[df_start['start'].str.contains(str(
-                                                                    df_start.iloc[int(df_start.shape[0]) - 1]['start'][
-                                                                    0:-3]))].mean()), 2)) + ' USD' + \
-                      '\nMean for prev 100 days (not first days of the month): ' \
-                      '{:>5}'.format(str(round(float(avg_not_start), 2))) + ' USD' + \
-                      '\nMean for prev 1st days of months: ' \
-                      '{:>8}'.format(str(round(float(avg_start_only), 2))) + ' USD'
-            # send_slack_message(message, 0)
+            today_date = str(df_not_first.loc[int(df_not_first.shape[0] - 1)]['start'])
+            today_spent = str(round(float(df_not_first.loc[int(df_not_first.shape[0] - 1)]['amount']),
+                                    2))
+            prev_date = str(df_not_first.loc[int(df_not_first.shape[0] - 2)]['start'])
+            prev_spent = str(round(float(df_not_first.loc[int(df_not_first.shape[0] - 2)]['amount']), 2))
+            month_mean = round(float(df_start[df_start['start'].str.contains(
+                str(df_start.iloc[int(df_start.shape[0]) - 1]['start'][0:-3]))].mean()), 2)
+            mean_prev_first = str(round(float(avg_start_only), 2))
+            mean_not_first = str(round(float(avg_not_start), 2))
+            at_moment_time = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            len_last_100_without_first = int(df_not_first.shape[0])
+            len_last_100_first = int(df_start_first_only.shape[0])
+
+            message = f"{at_moment_time} \nNew S3 spends request. \n\nToday {today_date} spent: {today_spent}" \
+                f" USD. (Not first day of the month) \nYesterday: {prev_date}. Spent: {prev_spent} USD" \
+                f"\nMean for this month {last_response_month}: {month_mean} USD" \
+                f"\nMean for prev {len_last_100_without_first} days (not first days of the month): {mean_not_first} USD" \
+                f"\nMean for prev {len_last_100_first} 1st days of months: {mean_prev_first} USD"
+            send_slack_message(message, 0)
             send_email_message(message)
-            print(message)
             if last_response_amount > avg_not_start * 1.1:
                 if last_response_amount > avg_not_start * 1.2:
-                    alarm_message = message + '\n\nAlarm! average daly limit {} USD exceeded by {}%, now –– {} USD'.format(
-                        avg_not_start, round(float((last_response_amount / avg_not_start) * 100 - 100), 2),
-                        last_response_amount)
+                    exceed_percent = round(float((last_response_amount / avg_not_start) * 100 - 100), 2)
+                    alarm_message = message + f"\n\nAlarm! average daily limit {avg_not_start} USD exceeded by " \
+                        f"{exceed_percent}%, now –– {last_response_amount} USD."
                     send_slack_message(alarm_message, 1)
                     send_email_message(alarm_message)
                 else:
-                    warning_message = message + '\n\nWarning! average daily limit {} USD exceeded by {}%, now –– {} USD'.format(
-                        avg_not_start, round(float((last_response_amount / avg_not_start) * 100 - 100), 2),
-                        last_response_amount)
+                    exceed_percent = round(float((last_response_amount / avg_not_start) * 100 - 100), 2)
+                    warning_message = message + f"\n\nWarning! average daily limit {avg_not_start} USD exceeded by " \
+                        f"{exceed_percent}%, now –– {last_response_amount} USD."
                     send_slack_message(warning_message, 1)
                     send_email_message(warning_message)
     return last_100_list
@@ -251,15 +261,13 @@ def upload_response_list_to_s3(response_list: list):
     s3_key = os.environ['S3_KEY']
     s3_bucket = os.environ['S3_BUCKET']
     obj = s3.Object(s3_bucket, s3_key)
-    print('dumped list=', json.dumps(response_list))
-    print('type of dumped list', type(json.dumps(response_list)))
     obj.put(Body=json.dumps(response_list))
     print('Uploaded new file to s3')
 
 
 def lambda_handler(event, context):
     file_from_s3 = read_s3_file()
-    list_of_100 = read_last_100(file_from_s3, 100)
+    list_of_100 = read_last_100(file_from_s3, '2020-05-10')
     analyzed_list_of_100 = analyze_w_last100(list_of_100)
     upload_response_list_to_s3(analyzed_list_of_100)
 
